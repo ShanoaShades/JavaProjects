f698be5565d3ad2c198dbed5cf568617eb20ec67
Shanoa <shanoa.shades@gmail.com>
2022-12-09 11:48:52 +0100

    Convertisseur de devises - Avancement
    Je suis allée de bug en bug mais on progresse, je verrai lundi.

--- OrifLab/src/testChange/Bouton.java
+++ OrifLab/src/testChange/Bouton.java
@@ -1,5 +1,31 @@
 package testChange;
 
-public class Bouton {
-	// Test
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+
+
+public class Bouton extends Component {
+		// Bien hériter de Component pour pouvoir l'utiliser après avec la méthode add de Fenetre.
+			
+		public Bouton() {
+		super();
+		
+		JButton calculer = new JButton("EFFACER");
+		calculer.setBounds(0, 50, 95, 30);
+		calculer.setBackground(Color.BLUE);
+		calculer.setOpaque(true);
+		calculer.setForeground(Color.WHITE);
+		calculer.setFont(new Font("Verdana", Font.BOLD, 10));
+		
+		JButton effacer = new JButton("EFFACER");
+		effacer.setBounds(0, 100, 95, 30);
+		effacer.setBackground(Color.BLUE);
+		effacer.setOpaque(true);
+		effacer.setForeground(Color.WHITE);
+		effacer.setFont(new Font("Verdana", Font.BOLD, 10));
+	}
+
 }
