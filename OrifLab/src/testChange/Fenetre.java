6384a6953c2d55331dd98ebe67e1079195ba8efe
Shanoa <shanoa.shades@gmail.com>
2022-12-19 11:58:21 +0100

    Bloquée getSource()
    La mise en forme du Layout est terminée, j'ai réussi à gérer les deux text field pour que l'un devienne non sélectionnable et pas l'autre en fonction de la checkBox sélectionnée. Mais le getsource() me pose problème pour mes ActionListener.
    
    Je verrai demain avec Najib.

--- OrifLab/src/testChange/Fenetre.java
+++ OrifLab/src/testChange/Fenetre.java
@@ -1,12 +1,14 @@
 package testChange;
 
 import javax.swing.*;
-
 import java.awt.Color;
 import java.awt.event.*;
 
-public class Fenetre implements ActionListener {
-	/** Cette classe s'occupe de la gestion du placement des différents objets ainsi que des actions exécutées lorsque les boutons seront appuyés. */
+public class Fenetre implements ActionListener{
+	/**
+	 * Cette classe s'occupe de la gestion du placement des différents objets ainsi
+	 * que des actions exécutées lorsque les boutons seront appuyés.
+	 */
 	JFrame fenetre;
 	JLabel chfL, euroL;
 	JTextField chfTF, euroTF;
@@ -17,39 +19,55 @@ public class Fenetre implements ActionListener {
 	Fenetre() {
 		// LABELS
 		JLabel chfL = new JLabel("CHF");
-		chfL.setBounds(6,0,75,30);
+		chfL.setBounds(6, 0, 75, 30);
 		JLabel euroL = new JLabel("EURO");
-		euroL.setBounds(396,0,75,30);
-		
+		euroL.setBounds(396, 0, 75, 30);
+
 		// CHAMPS DE TEXTE
 		JTextField chfTF = new JTextField();
-		chfTF.setBounds(33,6,85,20);
+		chfTF.setBounds(32, 6, 93, 20);
+		chfTF.setEditable(true);
 		JTextField euroTF = new JTextField();
-		euroTF.setBounds(306,6,85,20);
-		
+		euroTF.setBounds(300, 6, 93, 20);
+		euroTF.setEditable(false);
+
 		// BOUTONS RADIO
-		JRadioButton chfToEuroRB = new JRadioButton("CHF => €");
-		chfToEuroRB.setBounds(135,0,75,38);
+		JRadioButton chfToEuroRB = new JRadioButton("CHF => €", true);
+		chfToEuroRB.setBounds(135, 0, 75, 34);
 		JRadioButton euroToChfRB = new JRadioButton("€ => CHF");
-		euroToChfRB.setBounds(215,0,75,38);
-		// On place les deux boutons dans un groupe afin de pouvoir choisir entre l'un et l'autre au sein du même groupe.
-		ButtonGroup sensDeConversion = new ButtonGroup();    
+		euroToChfRB.setBounds(214, 0, 75, 34);
+		// On place les deux boutons dans un groupe.
+		ButtonGroup sensDeConversion = new ButtonGroup();
 		sensDeConversion.add(chfToEuroRB);
-		sensDeConversion.add(euroToChfRB);  
-		
+		sensDeConversion.add(euroToChfRB);
+		// ITEM LISTENERS DES CHECK BOXES
+		// Afin de swap le textfield qui est sélectionnable ou non.
+		chfToEuroRB.addItemListener(new ItemListener() {    
+            public void itemStateChanged(ItemEvent e) {                 
+            	chfTF.setEditable(true);
+				euroTF.setEditable(false);
+            }    
+         });
+		euroToChfRB.addItemListener(new ItemListener() {    
+            public void itemStateChanged(ItemEvent e) {                 
+            	chfTF.setEditable(false);
+				euroTF.setEditable(true);
+            }    
+         });   
+
 		// BOUTONS
 		Bouton calculer = new Bouton("CALCULER", Color.BLUE, 125, 52, 92, 24);
 		calculer.addActionListener(this);
 		Bouton effacer = new Bouton("EFFACER", Color.BLACK, 222, 52, 86, 24);
 		effacer.addActionListener(this);
-		
+
 		// FENETRE
 		fenetre = new JFrame("Convertisseur de devises");
 		fenetre.setSize(450, 120);
 		fenetre.setLayout(null);
 		fenetre.setVisible(true);
 		fenetre.setResizable(false);
-		
+
 		// AJOUT DES ÉLÉMENTS
 		fenetre.add(chfL);
 		fenetre.add(euroL);
@@ -59,9 +77,44 @@ public class Fenetre implements ActionListener {
 		fenetre.add(euroToChfRB);
 		fenetre.add(calculer);
 		fenetre.add(effacer);
-	}
 
+	}
+	
+	@Override
 	public void actionPerformed(ActionEvent e) {
+		System.out.println(e.getSource());
+		if(e.getSource() == calculer){ 
+			System.out.println("Saaaaaaaaaaaaaaaaaaaaluuuuuuuuuuuuuuut");
+			
+			String devise, resultat;
+			double tauxChange, deviseAConvertir, deviseConvertie;
+			
+			if(chfToEuroRB.isSelected()){    
+				tauxChange = 0.967328;
+				
+				devise = chfTF.getText();
+				deviseAConvertir = Double.parseDouble(devise);
+				deviseConvertie = deviseAConvertir * tauxChange;
+				
+				resultat = String.valueOf(deviseConvertie);  
+		        euroTF.setText(resultat);
+				}   
+			if(euroToChfRB.isSelected()){    
+				tauxChange = 1.033775;
+				
+				devise = euroTF.getText();
+				deviseAConvertir = Double.parseDouble(devise);
+				deviseConvertie = deviseAConvertir * tauxChange;
+				
+				resultat = String.valueOf(deviseConvertie);  
+				chfTF.setText(resultat);
+				}  
+        } else if(e.getSource() == effacer){
+        	System.out.println("YEAH");
+			chfTF.setText("POUET");
+			euroTF.setText("ça marche on dirait");
+		}
+		   
 		/*
 		 * firstName = fieldFirstName.getText(); name = fieldName.getText(); age =
 		 * fieldAge.getText(); section = fieldSection.getText();
